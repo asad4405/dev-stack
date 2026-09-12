f1a408f7ee2ea5c21e274ba8876172f7060475be
asad4405 <asadrifat450@gmail.com>
2026-09-12 12:20:12 +0600

    add data useing ai and added type

---
+++ src/types/TechnologyType.ts
@@ -0,0 +1,11 @@
+export interface TechonologyType{
+    id: number;
+    name: string;
+    image: string;
+    badge: string;
+    badgeColor: string;
+    description: string;
+    category: string;
+    level: string;
+    rating: number;
+}
